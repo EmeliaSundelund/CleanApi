0208339d3d0653e946cccaf6931f75f8fb4df7b2
EmeliaSundelund <Emelia_sundelund@hotmail.com>
2023-11-25 01:10:03 +0100

    gjort tester

---
+++ Test/DogTests/CommandTest/AddDogTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Application.Commands.Dogs;
+using Application.Dtos;
+using Domain.Models;
+using Infrastructure.Database;
+
+namespace Test.DogTests.CommandTest
+{
+    [TestFixture]
+    public class AddDogTests
+    {
+        private AddDogCommandHandler _handler;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _handler = new AddDogCommandHandler(new MockDatabase());
+        }
+
+        [Test]
+        public async Task AddsDogToDatabas()
+        {
+            var newDog = new DogDto { Name = "NewDogName" };
+            var command = new AddDogCommand(newDog);
+
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<Dog>(result);
+
+            Assert.That(result.Id, Is.Not.EqualTo(Guid.Empty));
+
+            Assert.That(result.Name, Is.EqualTo("NewDogName"));
+        }
+    }
+}
---
+++ Test/DogTests/QueryTest/GetAllDogsTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Application.Queries.Dogs;
+using Application.Queries.Dogs.GetAll;
+using Domain.Models;
+using Infrastructure.Database;
+
+namespace Test.DogTests.QueryTest
+{
+    [TestFixture]
+    public class GetAllDogsTests
+	{
+        private GetAllDogsQueryHandler _handler;
+        private MockDatabase _mockDatabase;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockDatabase = new MockDatabase();
+            _handler = new GetAllDogsQueryHandler(_mockDatabase);
+        }
+
+        [Test]
+        public async Task IfAllDogsReturnsCorrect()
+        {
+            var query = new GetAllDogsQuery();
+
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<List<Dog>>(result);
+            Assert.Greater(result.Count, 0);
+        }
+    }
+}
+
+
