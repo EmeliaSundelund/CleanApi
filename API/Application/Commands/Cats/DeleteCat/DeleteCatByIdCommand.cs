c230bf64a1e4440a03501eaee8c5b860d500fffd
Emelia Sundelund <105558496+EmeliaSundelund@users.noreply.github.com>
2023-11-28 19:16:41 +0100

    Add files via upload

---
+++ API/Application/Commands/Cats/DeleteCat/DeleteCatByIdCommand.cs
@@ -0,0 +1,20 @@
+﻿using Application.Commands.Cats.AddCat;
+using Application.Dtos;
+using Domain.Models;
+using Infrastructure.Database;
+using MediatR;
+
+namespace Application.Commands.Cats.DeleteCat
+{
+    public class DeleteCatByIdCommand : IRequest<bool>
+    {
+        public DeleteCatByIdCommand(CatDto deletedCat, Guid deletedCatId)
+        {
+            DeletedCat = deletedCat;
+            DeletedCatId = deletedCatId;
+        }
+
+        public CatDto DeletedCat { get; }
+        public Guid DeletedCatId { get; }
+    } //kommentar
+}
