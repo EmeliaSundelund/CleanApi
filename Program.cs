fca3252cdff52a48af9804c6e7a3deb4c9db9f40
Emelia Sundelund <105558496+EmeliaSundelund@users.noreply.github.com>
2023-12-20 19:20:30 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,99 @@
+using System.Text;
+using API.Controllers.Token;
+using Application;
+using Infrastructure;
+using Infrastructure.DataDbContex;
+using Infrastructure.DataDbContex.Interfaces;
+using Infrastructure.DataDbContex.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+namespace YourNamespace
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+    
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddApplication().AddInfrastructure();
+
+
+            builder.Services.AddDbContext<DataDbContex>(options =>
+            {
+                var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString),
+                    b => b.MigrationsAssembly("Infrastructure"));
+            });
+
+  
+            builder.Services.AddScoped<IUserInterface, UsersRepository>();
+
+ 
+            builder.Services.AddTransient<IAnimalUserRepository, AnimalUserRepository>();
+
+            builder.Services.AddScoped<ITokenService, TokenService>();
+
+        
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Skriv Bearer och sen token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        System.Array.Empty<string>()
+                    }
+                });
+            });
+
+            
+            builder.Services.AddAuthentication().AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("AppSettings:TokenKey").Value)),
+
+                };
+            });
+
+            var app = builder.Build();
+
+    
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting(); 
+
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+
+        }
+    }
+}
